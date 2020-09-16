08f7187c21b69d85f6d72dc5ce8a9018b91c3c44
dsouzajude <dsouza_jude@hotmail.com>
2020-09-16 23:10:34 +0200

    Add ALB to Ingress GW

--- terraform/demo/ingress-gw.tf
+++ terraform/demo/ingress-gw.tf
@@ -14,6 +14,7 @@ locals {
     {
       region = local.aws_region
 
+      service_name            = local.ingress_gw["service_name"]
       envoy_gw_image          = local.ingress_gw["envoy_gw_image"]
       envoy_port              = local.ingress_gw["envoy_port"]
       envoy_process_port      = local.ingress_gw["envoy_process_port"]
@@ -60,6 +61,108 @@ module "ingress_gw_service" {
   ]
   subnet_ids = var.subnet_ids
   tags       = merge(var.tags, local.ingress_gw_tags)
+
+  target_groups = [
+    {
+      target_group_arn = aws_alb_target_group.ingress_gw.arn
+      container_name   = local.ingress_gw["service_name"]
+      container_port   = local.ingress_gw["envoy_port"]
+    }
+  ]
+}
+
+# --------------------------
+#  Application Load Balancer
+# --------------------------
+
+resource "random_id" "postfix" {
+  byte_length = 5
+}
+
+resource "aws_lb" "ingress_gw" {
+  name                       = "int-alb-${local.ingress_gw["service_name"]}-${random_id.postfix.hex}"
+  internal                   = true
+  load_balancer_type         = "application"
+  security_groups            = [aws_security_group.ingress_gw_alb.id]
+  subnets                    = var.subnet_ids
+  enable_deletion_protection = false
+  idle_timeout               = 60
+  tags                       = merge(var.tags, local.ingress_gw_tags)
+}
+
+resource "aws_lb_listener" "ingress_gw" {
+  load_balancer_arn = aws_lb.ingress_gw.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.ingress_gw.arn
+  }
+}
+
+resource "aws_alb_target_group" "ingress_gw" {
+  name                 = "${local.ingress_gw["service_name"]}-${random_id.postfix.hex}"
+  port                 = local.ingress_gw["envoy_port"]
+  protocol             = "HTTP"
+  vpc_id               = var.vpc_id
+  target_type          = "ip"
+  deregistration_delay = 30
+  slow_start           = 30
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    port                = local.ingress_gw["envoy_port"]
+    path                = "/health"
+    interval            = "5"
+    timeout             = "4"
+    matcher             = "404" // Currently health checks are not supported on Consul Ingress Gateways
+  }
+
+  tags = merge(var.tags, local.ingress_gw_tags)
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# ----------------
+#  Security Groups
+# ----------------
+
+resource "aws_security_group" "ingress_gw_alb" {
+  name        = "ingress-gw"
+  description = "Ingress Gateway SG"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "Allow HTTP inbound connectivity to Ingress"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description     = "Allow egress to ECS Services"
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = [aws_security_group.ecs_service_sg.id]
+  }
+
+  tags = merge(var.tags, local.ingress_gw_tags)
+}
+
+// Allow inbound from Ingress GW to ECS Service
+resource "aws_security_group_rule" "allow_ingress_gw_inbound_to_ecs_service" {
+  type                     = "ingress"
+  description              = "Allow Inbound connectivity from Ingress GW to ECS Service"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  security_group_id        = aws_security_group.ecs_service_sg.id
+  source_security_group_id = aws_security_group.ingress_gw_alb.id
 }
 
 # ------
